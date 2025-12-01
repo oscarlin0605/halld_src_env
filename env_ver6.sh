53b8e1522989bae4a93f5a8d3e1954c19c1628ad
Oscar <lin1137@mit.edu>
2025-12-01 11:03:19 -0500

    add 6.5.0

---
+++ env_ver6.sh
@@ -0,0 +1,4 @@
+export BUILD_SCRIPTS="/group/halld/Software/build_scripts"
+export BMS_OSNAME=`$BUILD_SCRIPTS/osrelease.pl`
+source $BUILD_SCRIPTS/gluex_env_boot_jlab.sh
+gxenv /home/oscarlin/env/gx_version/version_6.5.0.xml
